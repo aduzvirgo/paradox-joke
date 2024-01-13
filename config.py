6cf69022a732016c24f1d0230f71a0e279107adc
Paradox <solomonadonay1@gmail.com>
2024-01-13 19:40:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #pass your tokens here 
-BOT_TOKEN ='6028947050:AAHQqySDT8zRIIgMgWBTYUaSs7YcN6q8hwg' # bot token from t.me/botfather
-API_ID = 9583161 #api id from telegram.org
-API_HASH = 'aecf9e2b7c655c4f916564ab6d598a73' #api hash from telegram.org
+BOT_TOKEN ='6744313937:AAGgmpbUm1TjLY_4RrI6rh81ygsxwju5tn0' # bot token from t.me/botfather
+API_ID = 28153993 #api id from telegram.org
+API_HASH = '976fd7cc4958ad84181a53b41919564b' #api hash from telegram.org
     
