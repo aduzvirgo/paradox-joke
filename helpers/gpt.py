b910aba2ab4609b2495c0fd302f2a57c91ffcda3
Paradox <solomonadonay1@gmail.com>
2024-01-13 20:04:23 +0300

    Update gpt.py

--- helpers/gpt.py
+++ helpers/gpt.py
@@ -1,12 +1,26 @@
-
 import requests
 
-def gpt(text):
-    url = "http://216.98.10.228:5000/gpt"
-    params = {
-    "ask":text}
-    response = requests.get(url, params=params)
-    data = response.json()
-    return data['answer']
+const axios = require('axios');
 
-print(data)
+const options = {
+  method: 'GET',
+  url: 'https://ronreiter-meme-generator.p.rapidapi.com/meme',
+  params: {
+    top: 'Top Text',
+    bottom: 'Bottom Text',
+    meme: 'Condescending-Wonka',
+    font_size: '50',
+    font: 'Impact'
+  },
+  headers: {
+    'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+    'X-RapidAPI-Host': 'ronreiter-meme-generator.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
