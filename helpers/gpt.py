61cd739070180bd012b0fc8ec446c02e48d03e0b
Paradox <solomonadonay1@gmail.com>
2024-01-13 20:12:19 +0300

    Update gpt.py

--- helpers/gpt.py
+++ helpers/gpt.py
@@ -2,11 +2,17 @@ import requests
 
 url = "https://ronreiter-meme-generator.p.rapidapi.com/meme"
 
-querystring = {"top":"Top Text","bottom":"Bottom Text","meme":"Condescending-Wonka","font_size":"50","font":"Impact"}
+querystring = {
+    "top": "Top Text",
+    "bottom": "Bottom Text",
+    "meme": "Condescending-Wonka",
+    "font_size": "50",
+    "font": "Impact"
+}
 
 headers = {
-	"X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
-	"X-RapidAPI-Host": "ronreiter-meme-generator.p.rapidapi.com"
+    "X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
+    "X-RapidAPI-Host": "ronreiter-meme-generator.p.rapidapi.com"
 }
 
 response = requests.get(url, headers=headers, params=querystring)
